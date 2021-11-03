795e4aff2495852f0229ddf34ab2bc751acd00a0
turkalpp <71527921+turkalpp@users.noreply.github.com>
2021-11-03 22:14:42 +0300

    Add files via upload

---
+++ area and perimeter of rectangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int width,length;
+	cout<<"Enter the width of the rectangle: ";
+	cin>>width;
+	cout<<"Enter the length of the rectangle: ";
+	cin>>length;
+	cout<<"Perimeter:"<<width+width+length+length<<endl;
+	cout<<"Area:"<<width*length;
+}
